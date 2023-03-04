5307fb1426ba648ce9c3744c47d4ca371a58af1f
AhmedKassem <ahmedkassem996@gmail.com>
2023-03-04 20:25:09 +0200

    Add .gitignore file

---
+++ CompleteSwiftUIProject/BackgroundThreads.swift
@@ -0,0 +1,20 @@
+//
+//  BackgroundThreads.swift
+//  CompleteSwiftUIProject
+//
+//  Created by Ahmed Kasem on 03/03/2023.
+//
+
+import SwiftUI
+
+struct BackgroundThreads: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BackgroundThreads_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundThreads()
+    }
+}
---
+++ CompleteSwiftUIProject/CodableSui.swift
@@ -0,0 +1,20 @@
+//
+//  CodableSui.swift
+//  CompleteSwiftUIProject
+//
+//  Created by Ahmed Kasem on 04/03/2023.
+//
+
+import SwiftUI
+
+struct CodableSui: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CodableSui_Previews: PreviewProvider {
+    static var previews: some View {
+        CodableSui()
+    }
+}
